b6d6cc620b02cb82c1a2dad4eb68322f8876c04c
LeanIsaac <leancarp09@gmail.com>
2026-05-19 00:53:10 -0300

    feat: agrupa controles para el formulario del registro

--- src/app/components/registro/registro.ts
+++ src/app/components/registro/registro.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Iregistro } from '../../models/auth.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
-export class Registro {}
+export class Registro {
+  authService = inject(AuthService);
+
+  formulario = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    nombre: new FormControl('', [Validators.required]),
+    apellido: new FormControl('', [Validators.required]),
+    edad: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  accion(){
+    this.authService.registrar(this.formulario.value as Iregistro);
+  }
+}
