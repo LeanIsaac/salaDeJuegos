591d18f0018c7dc62160260b07845badb74ccf8a
LeanIsaac <leancarp09@gmail.com>
2026-05-20 01:51:10 -0300

    feat: agrega componentes de los demas juegos

---
+++ src/app/components/juegos/blackjack/blackjack.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blackjack',
+  imports: [],
+  templateUrl: './blackjack.html',
+  styleUrl: './blackjack.css',
+})
+export class Blackjack {}
