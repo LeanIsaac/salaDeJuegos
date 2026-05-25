896fd6023a7988d207a5096713f2f0f1015d36e6
LeanIsaac <leancarp09@gmail.com>
2026-05-25 19:47:13 -0300

    feat: agrega la logica para el mayor-menor

--- src/app/components/juegos/mayor-menor/mayor-menor.ts
+++ src/app/components/juegos/mayor-menor/mayor-menor.ts
@@ -1,9 +1,70 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal} from '@angular/core';
+
 
 @Component({
   selector: 'app-mayor-menor',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mayor-menor.html',
   styleUrl: './mayor-menor.css',
 })
-export class MayorMenor {}
+
+export class MayorMenor implements OnInit{
+  cartaActual: number = 0;
+  cartaSiguiente: number = 0;
+  puntaje: number = 0;
+  mensaje: string = '';
+  juegoActivo: boolean = false;
+  revelando = signal<boolean>(false);
+
+  ngOnInit() {
+    this.reiniciarJuego();
+  }
+
+  reiniciarJuego() {
+    this.cartaActual = this.obtenerCarta();
+    this.juegoActivo = true;
+    this.puntaje = 0;
+    this.mensaje = '';
+    this.revelando.set(false);
+  }
+
+  obtenerCarta(): number {
+    return Math.floor(Math.random() * 12) + 1;
+  }
+
+  async adivinarCarta(opcion: 'mayor' | 'menor') {
+
+    if (!this.juegoActivo) {
+      return;
+    }
+
+    this.cartaSiguiente = this.obtenerCarta();
+    this.revelando.set(true);
+
+    const esMayor = this.cartaSiguiente > this.cartaActual;
+    const esMenor = this.cartaSiguiente < this.cartaActual;
+
+    // Evaluo si el jugador acertó
+    const acerto = (opcion === 'mayor' && esMayor) || (opcion === 'menor' && esMenor);
+
+    if (acerto) {
+      this.puntaje++;
+      this.mensaje = '¡Correcto!';
+    } else {
+      this.mensaje = 'Incorrecto. Fin del juego.';
+    }
+
+    // Pauso la ejecución por 1 segundo
+    // Esto le da tiempo al usuario de ver la carta renderizada en el HTML (?
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    this.revelando.set(false);
+
+    if (acerto) {
+      this.cartaActual = this.cartaSiguiente;
+      this.mensaje = '';
+    } else {
+      this.juegoActivo = false;
+    }
+  }
+}
